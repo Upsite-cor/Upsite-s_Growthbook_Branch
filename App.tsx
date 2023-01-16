5bb7e072b22e1ac371f5f68684268890901d1a42
Umer Naeem <mumer.naeem217@gmail.com>
2023-01-16 13:22:48 -0800

    inital commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Growthbook</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
