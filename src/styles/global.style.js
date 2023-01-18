ad576b2546cc3285ae569d9abc0afb701cffd552
Umer Naeem <mumer.naeem217@gmail.com>
2023-01-17 17:11:34 -0800

    font changes done

---
+++ src/styles/global.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const globalStylesheet= StyleSheet.create({
+    centered:{
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    //Typogrpahy
+    heading:{
+        
+    }
+})
+
+export default globalStylesheet;
